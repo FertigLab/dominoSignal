8b7c589619bb610736b7081cddad9c82dfceca96
Jacob Mitchell <jmitch81@jhmi.edu>
2024-07-22 09:21:23 -0400

    initial test circos_shape_data

---
+++ tests/testthat/test-circos.R
@@ -0,0 +1,10 @@
+test_that("Creating a circos plot data frame", {
+  data(PBMC)
+  arc_df <- circos_lr_shape_data(
+    dom = v0.2.1$pbmc_dom_built_tiny, 
+    receptor="CXCR3", 
+    ligand_expression_threshold = 0.01, cell_idents = NULL
+  )
+  expect_true(is(arc_df, "data.frame"))
+  
+})
