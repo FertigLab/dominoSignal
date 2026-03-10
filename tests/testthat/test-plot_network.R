2c03c708691f8931db435743fd140216e9b89107
kjkrishnan <kkrishnan@som.umaryland.edu>
2026-03-10 15:17:05 -0400

    Fix bug in signaling_plot Addresses NA values created by scaling in signaling_plot Changed warning to error if no signaling is found with parameters entered May help to resolve #155 (if the NAs were causing that issue, which is not totally clear)

--- tests/testthat/test-plot_network.R
+++ tests/testthat/test-plot_network.R
@@ -3,11 +3,22 @@ test_that("signaling_network function runs", {
     expect_no_error(signaling_network(tiny_dom1, edge_weight = 0.1, scale = "none", normalize = "none"))
 })
 
+test_that("signaling_network returns error if no signaling is found", {
+    expect_error(signaling_network(tiny_dom1, showOutgoingSignalingClusts = "CD14_monocyte",
+            showIncomingSignalingClusts = "CD8_T_cell"))
+})
+
+test_that("signaling_network returns a graph with scaled vertices even if scaling results in NA values", {
+    expect_no_error(signaling_network(tiny_dom1, showOutgoingSignalingClusts = "CD8_T_cell",
+            scale_by = "lig_sig"))
+    expect_no_error(signaling_network(tiny_dom1, showIncomingSignalingClusts = "B_cell"))
+})
+
 test_that("gene_network function runs", {
     expect_no_error(gene_network(tiny_dom1, clust = levels(tiny_dom1@clusters)[1], layout = "grid"))
 })
 
-test_that("gene_network can handle multiple clusters for incoming/outgoing signaling", {
+test_that("gene_network can handle multiple clusters for incoming and outgoing signaling", {
     expect_no_error(gene_network(tiny_dom1, clust = levels(tiny_dom1@clusters)[1:2], 
             OutgoingSignalingClust = levels(tiny_dom1@clusters)[2:3], layout = "grid"))
     
